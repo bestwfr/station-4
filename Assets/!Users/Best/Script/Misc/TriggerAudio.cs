4fdac2828a2641e3bd09a2324ecef7fe348fe12e
Best <best.xp345@gmail.com>
2025-10-26 22:06:57 +0700

    Trigger audio ambient om building

---
+++ Assets/!Users/Best/Script/Misc/TriggerAudio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Users.Best.Script.Misc
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class TriggerAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioClip clip;
+        private AudioSource source;
+
+        private void Awake()
+        {
+            source = GetComponent<AudioSource>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            source.PlayOneShot(clip);
+        }
+    }
+}
